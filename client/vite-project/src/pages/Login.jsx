d7ad40a0c614974d9ae101f5927df0fa626354df
drleavio <drlevio123@gmail.com>
2025-05-29 23:31:04 +0530

    loader added

--- client/vite-project/src/pages/Login.jsx
+++ client/vite-project/src/pages/Login.jsx
@@ -2,10 +2,12 @@ import React, { useContext, useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import AuthContext from '../context/useAuth';
+import { toast } from "react-toastify";
 
 const Login = () => {
     const navigate = useNavigate();
     const { token, setTokenState } = useContext(AuthContext)
+    const [loading,setLoading]=useState(false)
     const [data, setData] = useState({
         username: "",
         password: ""
@@ -18,10 +20,13 @@ const Login = () => {
     }
     const handleclick = async (e) => {
         e.preventDefault();
+        setLoading(true)
         const response = await axios.post("https://ecommerce-app-1-gn2p.onrender.com/auth/signin", data);
         console.log(response.data.token);
         setTokenState(response.data.token)
+        toast.success("loggedin successfully")
         navigate('/')
+        setLoading(false)
     }
     useEffect(() => {
        if(token){
@@ -35,7 +40,7 @@ const Login = () => {
                 <h1 className='text-4xl'>Login</h1>
                 <input type="text" name='username' placeholder='username' className='w-[350px] border border-gray-300 p-2 rounded-xl' onChange={(e) => handleChange(e)} />
                 <input type="password" name='password' placeholder='password' className='w-[350px] border border-gray-300 p-2 rounded-xl' onChange={(e) => handleChange(e)} />
-                <button className='w-full bg-black text-white p-2 rounded-xl' onClick={(e) => handleclick(e)}>Login</button>
+                <button className='w-full bg-black text-white p-2 rounded-xl' onClick={(e) => handleclick(e)}>{loading?"loggingin...":"Login"}</button>
                 <p>Don't have an account? please <Link to="/signup">SignUp</Link></p>
             </div>
         </div>
--- client/vite-project/src/pages/SignUp.jsx
+++ client/vite-project/src/pages/SignUp.jsx
@@ -2,10 +2,12 @@ import axios from 'axios'
 import React, { useState,useEffect, useContext } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import AuthContext from '../context/useAuth';
+import { toast } from "react-toastify";
 
 const SignUp = () => {
     const navigate=useNavigate();
     const {token}=useContext(AuthContext)
+    const [loading,setLoading]=useState(false)
     const [data,setData]=useState({
         username:"",
         password:""
@@ -18,10 +20,12 @@ const SignUp = () => {
     }
     const handleclick=async(e)=>{
         e.preventDefault();
+        setLoading(true)
         const response=await axios.post("https://ecommerce-app-1-gn2p.onrender.com/auth/signup",data);
+        toast.success("signedup successfully")
         navigate('/login')
         console.log(response);
-        
+        setLoading(false)
     }
      useEffect(() => {
            if(token){
@@ -35,7 +39,7 @@ const SignUp = () => {
                 <h1 className='text-4xl'>SignUp</h1>
                 <input type="text" name='username' onChange={(e)=>handleChange(e)} placeholder='username' className='w-[350px] border border-gray-300 p-2 rounded-xl'/>
                 <input type="text" name='password' onChange={(e)=>handleChange(e)} placeholder='password'className='w-[350px] border border-gray-300 p-2 rounded-xl' />
-                <button className='w-full bg-black text-white p-2 rounded-xl' onClick={(e)=>handleclick(e)}>SignUp</button>
+                <button className='w-full bg-black text-white p-2 rounded-xl' onClick={(e)=>handleclick(e)}>{loading?"signingup...":"Signup"}</button>
                 <p>Already have an account? please <Link to="/login">Login</Link></p>
             </div>
     </div>
